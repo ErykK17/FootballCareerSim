18cee56a44c18183c8109abc62bea7cbd942cd0d
ErykK17 <erykrygier@gmail.com>
2026-08-07 06:47:52 +0200

    zmieniono logikie losowania koncowych statystyk

--- sim_tools.ts
+++ sim_tools.ts
@@ -1,4 +1,4 @@
-import { Footballer, Club, Position, Injury, INJURIES} from "./entities";
+import { Footballer, Club, Position, Injury, INJURIES } from "./entities";
 
 export const getRandomFloat = (min: number, max: number): number => {
     return Number((Math.random() * (max - min) + min).toFixed(2));
@@ -37,15 +37,15 @@ export const determinePlaytime = (footballer: Footballer, club: Club): number =>
 
 export const POSITION_GA_RATES: Record<string, [number, number]> = {
     "ST": [0.35, 0.10],
-    "LW": [0.25, 0.20],
-    "RW": [0.25, 0.20],
-    "CAM": [0.20, 0.20],
-    "CM": [0.10, 0.15],
-    "CDM": [0.05, 0.15],
-    "LB": [0.025, 0.10],
-    "RB": [0.025, 0.10],
-    "CB": [0.05, 0.025],
-}
+    "LW": [0.25, 0.2],
+    "RW": [0.25, 0.15],
+    "CAM": [0.20, 0.18],
+    "CM": [0.08, 0.10],
+    "CDM": [0.03, 0.05],
+    "LB": [0.02, 0.07],
+    "RB": [0.02, 0.07],
+    "CB": [0.035, 0.015],
+};
 
 export const baseGAPerGame = (position: Position): [number, number] => {
     return POSITION_GA_RATES[position.toUpperCase()] ?? [0.0, 0.0]
@@ -70,25 +70,25 @@ export const seasonalForm = (professionalism: number): SeasonalFormResult => {
         return { name: "Srednia", multiplier: 1.0, gamesPenalty: 0 };
     }
     if (form <= 90) {
-        return { name: "Dobra", multiplier: 1.2, gamesPenalty: 0 };
+        return { name: "Dobra", multiplier: 1.15, gamesPenalty: 0 };
     }
     if (form <= 98) {
-        return { name: "Bardzo Dobra", multiplier: 1.4, gamesPenalty: 0 };
+        return { name: "Bardzo Dobra", multiplier: 1.3, gamesPenalty: 0 };
     }
 
-    return { name: "Sezon Zycia", multiplier: 1.6, gamesPenalty: 0 };
+    return { name: "Sezon Zycia", multiplier: 1.45, gamesPenalty: 0 };
 };
 
-export const simulateInjury = (injuryRisk: number): Injury | null =>{
-    const injuryRoll = getRandomFloat(0,100);
-    if (injuryRoll <= injuryRisk){
-        let injurySeverityRoll = getRandomFloat(0,100);
-        for(const injury of Object.values(INJURIES)){
-            if (injurySeverityRoll <= injury.probability){
+export const simulateInjury = (injuryRisk: number): Injury | null => {
+    const injuryRoll = getRandomFloat(0, 100);
+    if (injuryRoll <= injuryRisk) {
+        let injurySeverityRoll = getRandomFloat(0, 100);
+        for (const injury of Object.values(INJURIES)) {
+            if (injurySeverityRoll <= injury.probability) {
                 return injury;
             }
             injurySeverityRoll -= injury.probability;
-            
+
         }
     }
     return null;
@@ -101,45 +101,85 @@ export const calculateGA = (position: Position, ovr: number, formMultiplier: num
     return { goals: xgoals, assists: xassists };
 }
 
-let ga = calculateGA("ST", 90, 1.6, 84, 76);
-console.log(ga);
 
 const samplePoisson = (lambda: number): number => {
     // Zabezpieczenie przed xG/xA <= 0
-    if (lambda <= 0) return 0; 
-
+    if (lambda <= 0) return 0;
+    
     const rand = Math.random();
     let k = 0;
     let p = Math.exp(-lambda); // P(K = 0)
     let cumulativeProbability = p;
-
+    
     while (rand > cumulativeProbability) {
         k++;
         p = p * (lambda / k);
         cumulativeProbability += p;
     }
-
+    
     return k;
 };
 
-// Główna funkcja symulująca bramki i asysty w meczu
 export const simulateMatchGA = (xG: number, xA: number): { goals: number; assists: number } => {
-    console.log(samplePoisson(xG));
+
+    const simulateGoals = (expectedValue: number): number => {
+        if (expectedValue <= 0) return 0;
+
+        const p1 = Math.min(expectedValue * 0.82, 0.95);
+
+        let m2 = 0.25; // Dublet
+        let m3 = 0.13; // Hat-trick
+        let m4 = 0.08; // Poker
+        let m5 = 0.03; // Manita
+
+        const p2 = p1 * m2;
+        const p3 = p2 * m3;
+        const p4 = p3 * m4;
+        const p5 = p4 * m5;
+
+        const roll = Math.random();
+
+        if (roll < p5) return 5;
+        if (roll < p4) return 4;
+        if (roll < p3) return 3;
+        if (roll < p2) return 2;
+        if (roll < p1) return 1;
+
+        return 0;
+    };
+
     return {
-        goals: samplePoisson(xG),
-        assists: samplePoisson(xA)
+        goals: simulateGoals(xG),
+        assists: simulateGoals(xA)
     };
 };
+let ga = calculateGA("ST", 81, 1, 76, 77);
+console.log(ga);
 
 console.log(ga.goals);
 console.log(ga.assists);
 let totalGoals = 0;
 let totalAssists = 0;
-for (let i = 0; i < 30; i++) {
-
+let i = 0;
+let Hattricks = 0;
+let quadruples = 0;
+let pentacles = 0;
+for (i = 0; i < 34; i++) {
     const matchResult = simulateMatchGA(ga.goals, ga.assists);
     totalGoals += matchResult.goals;
     totalAssists += matchResult.assists;
+    if (matchResult.goals >= 3) {
+        Hattricks++;
+    }
+    if (matchResult.goals >= 4) {
+        quadruples++;
+    }
+    if (matchResult.goals >= 5) {
+        pentacles++;
+    }
 }
-console.log(`Total Goals in 30 matches: ${totalGoals}`);
-console.log(`Total Assists in 30 matches: ${totalAssists}`);
+console.log(`Total Goals in 34 matches: ${totalGoals}, ${(totalGoals / 34).toFixed(2)} per match`);
+console.log(`Total Assists in 34 matches: ${totalAssists}, ${(totalAssists / 34).toFixed(2)} per match`);
+console.log(`Hattricks: ${Hattricks}`);
+console.log(`Quadruples: ${quadruples}`);
+console.log(`Pentacles: ${pentacles}`);
